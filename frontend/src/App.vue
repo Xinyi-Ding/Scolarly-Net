b7a1ed59eea2a2166fa8bf8804874dc232595f4a
s2476735 <y.liang-67@sms.ed.ac.uk>
2024-02-08 22:32:40 +0000

    frontend initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-config-provider namespace="ep">
+    <BaseHeader />
+    <div class="flex main-container">
+      <BaseSide />
+    </div>
+  </el-config-provider>
+</template>
+
+<style>
+#app {
+
+}
+
+.main-container {
+  /*height: calc(100vh - var(--ep-menu-item-height) - 3px);*/
+  height: 100vh;
+}
+</style>
