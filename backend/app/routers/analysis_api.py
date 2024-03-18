1284f1760b9a14b9b4eb58013329513625cb15a5
Kejia <k.zhang-61@sms.ed.ac.uk>
2024-03-18 18:56:31 +0000

    fix the bug

--- backend/app/routers/analysis_api.py
+++ backend/app/routers/analysis_api.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from pathlib import Path
 from ..services import analysis
 from ..services.catalog import save_parse_article
+import os
 
 
 # Initialize the API router with a prefix and tags
@@ -12,8 +13,10 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-papers_directory = Path('app') / 'data' / 'Papers'
+papers_directory = Path().cwd() / "app" / "tmp" / "Papers"
+xml_directory = Path().cwd() / "app" / "tmp" / "xml"
 papers_directory.mkdir(parents=True, exist_ok=True)
+xml_directory.mkdir(parents=True, exist_ok=True)
 
 
 @router.get("/")
@@ -23,6 +26,7 @@ async def root():
 
 @router.post("/upload/")
 async def upload_document(file: UploadFile = File(...)):
+    print(Path().cwd())
     try:
         file_location = papers_directory / Path(file.filename)
         print("File received")
@@ -31,15 +35,18 @@ async def upload_document(file: UploadFile = File(...)):
             file_object.write(content)
         await file.close()
         print("File saved to disk: ", file_location)
-        article = analysis.get_article_object(
-            analysis.get_extracted_xml(str(file_location), grobid_server="http://localhost:8070"))
+        xml_location = analysis.get_extracted_xml(str(file_location), grobid_server="http://localhost:8070")
+        article = analysis.get_article_object(xml_location)
         print("Article object created")
         articleVO = save_parse_article(article)
         print("Article saved to database")
         res = {"code": 200,
                "message": "File uploaded successfully",
-               "data": article.to_json(article_id=articleVO.article_id)
+               "data": article.to_json(article_id=122)
                }
+        os.remove(file_location)
+        os.remove(xml_location)
+        print("tmp files removed")
         return JSONResponse(status_code=200, content=res)
     except Exception as e:
         print(e)
